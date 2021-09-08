f182f63ece65deb112ce849ca7b93beff23bb3a0
samahhamed227 <samahhamed227@gmail.com>
2021-09-08 09:27:23 +0300

    amman

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const Server = express();
 const cors = require("cors");
 Server.use(cors());
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3003;
 const weather = require("./data/weather.json");
 require("dotenv").config();
 
@@ -12,35 +12,71 @@ Server.get("/", function (req, res) {
   res.send("Hello World in the home");
 });
 
-class forecast {
-  constructor(data, description) {
-    this.data = date;
+class Forecast {
+  constructor(date, description) {
+    this.date = date;
     this.description = description;
   }
 }
-
-Server.get("/get-weather", (req, res) => {
+//http://localhost:3003/weather?city_name=amman
+Server.get("/weather", (req, res) => {
   console.log(req.query);
-  const CityName = req.query.city_name;
+  const cityName = req.query.city_name;
 
-  let searchquery = weather.find((item) => {
-    return   (item.city_name === CityName) 
 
-  });
-  if (searchquery) {
-    let newarray = searchquery.data.map((item) => {
-      return new forecast(item.datetime, item.weather.description);
+
+  if (cityName) {
+    const returnArray = weather.find((item) => {
+      return item.city_name.toLowerCase() === cityName;
     });
-    res.json(newarray);
+   
+    let searchquery=returnArray.data.map((item) => {
+     
+      return new Forecast(item.datetime, item.weather.description);
+    });
+
+    
+    if (searchquery.length) {
+      res.json(searchquery);
+    } else {
+      res.send(" 404  error ");
+    }
+  } else {
+    res.json(weather);
   }
-
-  console.log(searchquery);
 });
 
-Server.get("*", (require, response) => {
-  response.send("not found");
-});
 
-Server.listen(PORT, () => {
-  console.log(`Server started on port${PORT}`);
-});
+Server.listen(PORT, () => {});
+
+
+
+
+
+
+
+
+
+
+
+//   let searchquery = weather.find((item) => {
+//     return   (item.city_name === CityName) 
+
+//   });
+//   if (searchquery) {
+//     let newarray = searchquery.data.map((item) => {
+//       return new Forecast(item.datetime, item.weather.description);
+//     });
+//     res.json(newarray);
+//   }
+
+//   console.log(searchquery);
+// });
+
+// Server.get("*", (require, res) => {
+//   res.send("not found");
+// });
+
+// Server.listen(PORT, () => {
+//   console.log(`Server started on port${PORT}`);
+// });
