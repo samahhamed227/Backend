7a475b5c48c45950b2848f89c45100a23746eb11
samahhamed227 <samahhamed227@gmail.com>
2021-09-15 20:42:18 +0300

    lab10

--- controller/movie.controller.js
+++ controller/movie.controller.js
@@ -1,15 +1,35 @@
-
 'use strict';
 const Movies = require('../modules/movie.modal');
 const axios=require('axios');
 require('dotenv').config();
-
+const Cache = require("../Cashe/cache");
+let cacheObject = new Cache();
 
 const MOVIES_API_KEY = process.env.MOVIE_key;
 
 const getMovie =  async (request, response) => {
  
   const city_name = request.query.query;
+
+
+
+  const shutTime = 65465;
+  const time = (Date.now() - cacheObject.timeStamp) > shutTime;
+  if (time) {
+   
+    cacheObject = new Cache();
+  }
+
+
+  const newData = cacheObject.movies.find(
+    (item) => item.city_name === city_name
+  );
+  if (newData) {
+    response.json(newData.data);
+  } else {
+
+
+
 
   const link = "https://api.themoviedb.org/3/search/movie";
   const linkResponse = await axios.get(
@@ -23,6 +43,13 @@ const getMovie =  async (request, response) => {
       return new Movies(item  );
     });
 
+    cacheObject.movies.push({
+      city_name: city_name,
+      data: movieArray,
+    });
+
+
+
     if (movieArray.length) {
       response.json(movieArray);
     } else {
@@ -31,5 +58,5 @@ const getMovie =  async (request, response) => {
   } else {
     response.json("Error");
   }
-}
+}}
 module.exports = getMovie;
--- controller/weather.controller.js
+++ controller/weather.controller.js
@@ -1,16 +1,32 @@
 
+
 const axios = require('axios');
 require('dotenv').config();
 const Forecast = require('../modules/weather.modal');
-
+const Cache = require("../Cashe/cache");
 const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
-
+let cacheObject = new Cache();
 
 
 const getWeather =  async (request, response) => {
  
   const city_name = request.query.city;
   
+  const timer = 30000;
+  const time = (Date.now() - cacheObject.timeStamp) > timer;
+  if (time) {
+   
+    cacheObject = new Cache();
+  }
+
+  const newData = cacheObject.forCast.find(
+    (item) => item.city_name === city_name
+  );
+  if (newData) {
+    response.json(newData.data);
+  } else {
+
+
   const link = "https://api.weatherbit.io/v2.0/forecast/daily";
   const linkResponse = await axios.get(
     `${link}?city=${city_name}&key=${WEATHER_API_KEY}`
@@ -25,6 +41,16 @@ const getWeather =  async (request, response) => {
       return new Forecast( item.weather.description, item.datetime    );
     });
 
+
+
+    cacheObject.forCast.push({
+      city_name: city_name,
+      data: weatherAraay,
+    });
+
+
+
+
     if (weatherAraay.length) {
       response.json(weatherAraay);
      
@@ -34,5 +60,5 @@ const getWeather =  async (request, response) => {
   } else {
     response.json("Error");
   }
-}
+}}
 module.exports = getWeather;
